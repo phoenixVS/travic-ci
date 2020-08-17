bc91c943679a92ac88ad5c44dd51fad6da8cf3e1
Valentin <valentin.vad.sh@gmail.com>
2020-08-18 00:22:27 +0300

    edited app.tsx again

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,7 @@ function App() {
   return (
     <div className="App">
     <header className="App-header">
-      <h1 className="App-title">I am Valentyn and i am testing travis ci.</h1>
+      <h1 className="App-title">I am testing travis ci.</h1>
     </header>
     <p className="App-intro">
       My name is vs_phoenix. Welcome, and hello!
