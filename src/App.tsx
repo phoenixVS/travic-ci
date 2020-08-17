1260a4794b0dc1d2851c6de99041b9c76593ef73
Valentin <valentin.vad.sh@gmail.com>
2020-08-18 00:45:49 +0300

    tests fixed

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,16 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-    <header className="App-header">
-      <h1 className="App-title">I am testing travis ci.</h1>
-    </header>
-    <p className="App-intro">
-      My name is vs_phoenix. Welcome, and hello!
-    </p>
-  </div>
+      <header className="App-header">
+        <h1 className="App-title">I am testing travis ci.</h1>
+      </header>
+      <p className="App-intro">
+        My name is vs_phoenix. Welcome, and hello!
+      </p>
+    </div>
   );
 }
 
