43595ae9c4388d2d41f617edcd525d5394c272f6
Valentin <valentin.vad.sh@gmail.com>
2020-08-18 00:09:57 +0300

    Added travis

--- src/App.tsx
+++ src/App.tsx
@@ -5,21 +5,13 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <header className="App-header">
+      <h1 className="App-title">Welcome To My Hello World!</h1>
+    </header>
+    <p className="App-intro">
+      My name is vs_phoenix. Welcome, and hello!
+    </p>
+  </div>
   );
 }
 
